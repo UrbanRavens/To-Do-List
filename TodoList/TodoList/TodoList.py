8ac76d18cba8fa33159d0378f6d0305a80db3e69
dlabbas <121309711+UrbanRavens@users.noreply.github.com>
2026-01-29 22:20:02 +0000

    Added a menu for the user to interact with, making procedures to match the users input and rejecting any invalid data, pulling the menu up again if this is the case

--- TodoList/TodoList/TodoList.py
+++ TodoList/TodoList/TodoList.py
@@ -1,5 +1,45 @@
 toDoList = []
-def add_task(task):
+def menu():
+    while True:
+        print("Todo List Menu:")
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Remove Task")
+        print("4. Exit")
+        choice = input("What would you like to do? ")
+        match choice:
+            case "1":
+                view_task()
+            case "2":
+                add_task()
+            case "3":
+                remove_task()
+            case "4":
+                print("Goodbye!")    
+                break
+            case _:
+                print("Invalid input, try again")
+
+def view_task():
+    if toDoList == []:
+        print("You currently have no tasks to view, add some next time!")
+    else:
+        print(toDoList)
+    
+                        
+def add_task():
+    task = input("What task would you like to add? ").capitalize()
     toDoList.append(task)
     print(f'Task "{task}" added to the list.')
-add_task(input("Enter a task to add: "))   
+
+def remove_task():
+    if toDoList == []:
+        print("You have no tasks that can be removed, add some next time!")
+    else:
+        task = input("What task would you like to remove?").capitalize()
+        toDoList.remove(task)
+        print(f'You have successfully removed "{task}" from the list.')    
+
+menu()
+
+
